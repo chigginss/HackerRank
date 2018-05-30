fd9169755fa550f3481261cc04f375eb37402f4b
Cierra Higgins <cierramhiggins@gmail.com>
2018-05-30 14:10:11 -0700

    third challenge

--- introduction_challenges.py
+++ introduction_challenges.py
@@ -55,3 +55,28 @@ if __name__ == '__main__':
     print (a + b)
     print (a - b)
     print (a * b)
+
+""" 
+Division 
+Read two integers and print two lines. The first line should contain integer division,  a//b . The second line should contain float division,  a/b .
+
+You don't need to perform any rounding or formatting operations.
+
+Input Format
+
+The first line contains the first integer, a . The second line contains the second integer, b.
+
+Output Format
+
+Print the two lines as described above."""
+
+from __future__ import division
+
+def division(a, b):
+    print int(round(a / b))
+    print a / b
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    division(a,b)
