0589d8f4c6bf496b6c64aa451c3bb1ed634fb2cd
Cierra Higgins <cierramhiggins@gmail.com>
2018-05-31 12:07:43 -0700

    sixth challenge

--- introduction_challenges.py
+++ introduction_challenges.py
@@ -138,6 +138,30 @@ def is_leap(year):
 
     return leap
 
+""" Read an integer N.
+
+Without using any string methods, try to print the following:
+
+123..N
+
+Note that "..." represents the values in between.
+
+Input Format
+
+The first line contains an integer N.
+
+Output Format
+
+Output the answer as explained in the task."""
+
+from __future__ import print_function
+print(*range(1, input() + 1), sep="")
+    
+if __name__ == '__main__':
+    n = int(raw_input())
+    
+
+
 
 
 
