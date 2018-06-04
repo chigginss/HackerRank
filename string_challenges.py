6f940892694a1ba300063e89eb50a5a54bf52dca
Cierra Higgins <cierramhiggins@gmail.com>
2018-06-04 15:09:28 -0600

    second sc

--- string_challenges.py
+++ string_challenges.py
@@ -16,3 +16,12 @@ def swap_case(s):
         new_string += letter
     
     return new_string
+
+"""You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python."""
+
+def print_full_name(a, b):
+    print "Hello {} {}! You just delved into python.".format(a, b)
+
+    
